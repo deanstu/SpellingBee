7af2120854373adb1899cd549bdc38d67d6ed013
Dean Stuart <dean.stuart@uvm.edu>
2021-12-03 16:15:33 -0500

    added dictionary edits to spellingbee.py

--- spellingbee.py
+++ spellingbee.py
@@ -4,26 +4,20 @@ Created on Wed Dec  1 16:52:10 2021
 @author: deanp
 """
 
+import pickle 
 
-letters = "noahtmp".lower()
-li = set(())
-for l in letters:
-    li.add(l)
-
-center = letters[0]
-
-with open('spelling_bee_dict.txt') as file:
-    dictionary = []
-    alphabet = set(())
-    
-    for line in file:
-        dictionary.append(line.strip())
-        alphabet.add(line[0])
+in_file = open('changing_dictionary.pkl', 'rb')
+dictionary = pickle.load(in_file)
+in_file.close()
+ 
+alphabet = set(())   
+for word in dictionary:
+    alphabet.add(word[0])
         
 def solver(letters, center):
     
     good = []
-    bad_letters = alphabet.difference(li)
+    bad_letters = alphabet.difference(letters)
     
     for word in dictionary:
         if center in word:
@@ -37,6 +31,34 @@ def solver(letters, center):
                 
     return good
 
+
+
+letters = input("what are the letters? ").lower()
+center = letters[0]
 words = solver(letters, center)
 for word in words:
     print(word)
+    
+response = input("any edits to the dictionary(y/n)? ").lower()
+if response == 'y':
+        
+    delete = input("remove any words from the dictionary(y/n)? ").lower()
+    while delete == 'y':
+        deletion = input("word to be removed: ").lower()
+        try:
+            dictionary.remove(deletion)
+        except:
+            print(deletion, " not in dictionary")
+        delete = input("any more removals(y/n)? ").lower()
+        
+    add = input("any additions to the dictionary(y/n)? ").lower()
+    while add == 'y':
+        addition = input("word to be added: ").lower()
+        if addition not in dictionary:
+            dictionary.append(addition)
+        else:
+            print("word already in dictionary")
+        add = input("any more additions(y/n)? ").lower()
+        
+          
+    pickle.dump(dictionary, open("changing_dictionary.pkl", "wb"))
